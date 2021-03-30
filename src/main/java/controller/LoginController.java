b33c17c0d91e354f3eddc10965d12c5ff4604a2f
lincolnmesatto <lincoln.mesatto@gmail.com>
2021-03-29 21:40:25 -0300

    versao inicial

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,19 @@
+package controller;
+
+public class LoginController {
+
+	private String nome;
+	
+	public void btnClick() {
+		getNome();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
